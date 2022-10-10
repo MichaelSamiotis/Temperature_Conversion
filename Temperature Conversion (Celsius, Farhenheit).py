6f906ad5064ac472518c435e8721d78b0649836d
Michael Samiotis <93983609+MichaelSamiotis@users.noreply.github.com>
2022-10-10 13:32:12 +0300

    Update Temperature Conversion (Celsius, Farhenheit).py

--- Temperature Conversion (Celsius, Farhenheit).py
+++ Temperature Conversion (Celsius, Farhenheit).py
@@ -1,27 +1,20 @@
 #Ask the user what conversion to do (C to F or the other way around?)
-while True:
-    conversion_input = input('Please enter C to convert Celsius to Fahrenheit or F to convert Fahrenheit to Celsius: ')
-
-#Adding an OR to exit the programme if input is a wrong value
-
-    if conversion_input in ['C', 'F']:
+conversion_input = 0
+while conversion_input not in ['C','F']:
+    conversion_input = input('Please enter C to convert Celsius to Fahrenheit or F to \
+convert Fahrenheit to Celsius: ')
 
 #Asking the number to be converted
-        converted_value = float(input('Please enter the {:s} amount to be converted: '.format(conversion_input)))
+converted_value = float(input(f'Please enter the {conversion_input} amount to be converted: '))
 
-        if conversion_input == 'C':
-            F = (converted_value * 1.80 + 32)
-            print('The amount of {:1.5f} Celsius degrees is {:1.5f} degrees of Fahrenheit'.format(converted_value, F))
-            break
-        else:
-            C = (converted_value - 32) / 1.80
-            print('The amount of {:1.5f} Fahrenheit degrees is {:1.5f} degrees of Celsius'.format(converted_value, C))
-            break
-    else:
-        print('Please enter a valid value: C or F')
-        continue
+if conversion_input == 'C':
+    F = (converted_value * 1.80 + 32)
+    print(f'The amount of {converted_value} Celsius degrees is {F} degrees of Fahrenheit')
+elif conversion_input == 'F':
+    C = (converted_value - 32) / 1.80
+    print(f'The amount of {converted_value} Fahrenheit degrees is {C} degrees of Celsius')
 
-exit_program = input("Press any key to exit")
-if exit_program :
-    print("Exiting the Program.")
+exit_program = input('Press any key to exit')
+if exit_program:
+    print('Exiting the Program.')
     exit()
